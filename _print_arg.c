1f136b08bfdf7cd7059f1f0c2edf29d596f7085f
mkhamisnab <mkhamisnabil@gmail.com>
2023-04-24 13:28:44 -0700

    Edited _print_arg.c

--- _print_arg.c
+++ _print_arg.c
@@ -3,6 +3,7 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
  * print_arg - This function handles the formatting of a single argument.
  * @specifier: The format specifier character (e.g. 'c', 's', '%', 'b').
@@ -13,44 +14,28 @@
  */
 void print_arg(char specifier, va_list args, char *buffer, int *buf_index)
 {
-	int j, number;
-	char *binary, *string, character;
-	char *adr = NULL, *temp_adr_buff;
+	int i = 0;
 
-	switch (specifier)
+	fmt_t fmt_types[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percent},
+		{'b', print_binary},
+		{'\0', NULL}};
+
+	while (fmt_types[i].specifier != '\0')
 	{
-	case 'c':
-		character = (char)va_arg(args, int);
-		buffer[(*buf_index)++] = character;
-		break;
-	case 's':
-		string = va_arg(args, char *);
-		for (j = 0; string[j] != '\0'; j++)
-			buffer[(*buf_index)++] = string[j];
-		break;
-	case '%':
-		buffer[(*buf_index)++] = '%';
-		break;
-	case 'b':
-		number = va_arg(args, int);
-		binary = cal_binary(number);
-		for (j = 0; binary[j] != '\0'; j++)
-			buffer[(*buf_index)++] = binary[j];
-		free(binary);
-		break;
-	case 'p':
-		adr = va_arg(args, void *);
-		temp_adr_buff = (char *)malloc(sizeof(adr) * 2 + 1);
-		sprintf(temp_adr_buff, "%p", adr);
-		for (j = 0; temp_adr_buff[j] != '\0'; j++)
-			buffer[(*buf_index)++] = temp_adr_buff[j];
-		free(temp_adr_buff);
-		break;
-	default:
+		if (fmt_types[i].specifier == specifier)
+		{
+			fmt_types[i].print_func(args, buffer, buf_index);
+			return;
+		}
+		i++;
+	}
+
 		if (specifier != ' ' && specifier != '\0')
 		{
 			buffer[(*buf_index)++] = '%';
 			buffer[(*buf_index)++] = specifier;
 		}
-	}
 }
