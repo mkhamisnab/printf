90073ad795b93aed773202f6cde84c4a03c0afca
ABDULMAJEED ADENIYI YUSUF <102981590+CodeMarshal007@users.noreply.github.com>
2023-04-25 21:16:50 +0100

    Update _print_string_functions.c

--- _print_string_functions.c
+++ _print_string_functions.c
@@ -76,3 +76,68 @@ void print_percent(va_list args, char *buffer, int *buf_index)
 	UNUSED(args);
 	buffer[(*buf_index)++] = '%';
 }
+/**
+ * print_reverse - prints a string in reverse order
+ * @args: The va_list of arguments to format.
+ * @buffer: A buffer to write the printed integer to
+ * @buf_index: A pointer to the current index of the buffer
+ *
+ * Return: void
+ */
+void print_reverse(va_list args, char *buffer, int *buf_index)
+{
+	char *s;
+	int len, i;
+	
+	s = va_arg(args, char *);
+	if (s == NULL)
+	{
+		char *nil_str = "(nil)";
+		while (*nil_str)
+			buffer[(*buf_index)++] = *nil_str++;
+		return;
+	}
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	for (i = len - 1; i >= 0; i--)
+		buffer[(*buf_index)++] = s[i];
+}
+/**
+ * print_rot13 - prints a string encoded with rot13 substitution cipher
+ * @args: The va_list of arguments to format.
+ * @buffer: A buffer to write the printed integer to
+ * @buf_index: A pointer to the current index of the buffer
+ *
+ * Return: void
+ */
+void print_rot13(va_list args, char *buffer, int *buf_index)
+{
+	char *s;
+	int i, j;
+	
+	char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char *alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	
+	s = va_arg(args, char *);
+	if (s == NULL)
+	{
+		char *nil_str = "(nil)";
+		while (*nil_str)
+			buffer[(*buf_index)++] = *nil_str++;
+		return;
+	}
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; alphabet[j]; j++)
+		{
+			if (s[i] == alphabet[j])
+			{
+				buffer[(*buf_index)++] = rot13[j];
+				break;
+			}
+		}
+		if (!alphabet[j])
+			buffer[(*buf_index)++] = s[i];
+	}
+}
